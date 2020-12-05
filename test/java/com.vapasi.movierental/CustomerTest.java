ec0c1c3452faa66b2b7b3569778788ef4db9c23d
sangeethavelan <sangeethavelan@WORKSPACES.THOUGHTWORKS.COM>
2020-12-05 20:54:07 +0530

    [Sangeetha] Added testcase for add rental code

--- test/java/com.vapasi.movierental/CustomerTest.java
+++ test/java/com.vapasi.movierental/CustomerTest.java
@@ -2,6 +2,7 @@ package com.vapasi.movierental;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
@@ -15,5 +16,21 @@ public class CustomerTest {
         String expectedOutput = null;
 
         assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void shouldReturnSuccessOnAddingRental() {
+
+        Customer customer = new Customer("Mark");
+        Movie movie = new Movie("Day of tommorrow",1);
+        Rental rental = new Rental(movie,2);
+        customer.addRental(rental);
+
+        String actualOutput = customer.statement();
+        String expectedOutput = null;
+
+        assertEquals(expectedOutput, actualOutput);
+
+
     }
 }
